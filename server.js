ce53e075e0354868b720377c509f391f4344bb93
unknown <thomastsiolis@hotmail.co.uk>
2021-10-01 10:06:11 +0100

    Fix heroku issue

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(DB, {
 
 
 
-const port = 3000;
-const server = app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
