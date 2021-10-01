c5130ae6ee048908b19dfff767e563a081f4fb73
unknown <thomastsiolis@hotmail.co.uk>
2021-10-01 09:38:43 +0100

    updated server file

--- server.js
+++ server.js
@@ -6,13 +6,20 @@ const app = require('./app');
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-mongoose.connect(
-	DB,
-	async(err)=>{
-			if(err) throw err;
-			console.log("DB Connection successful")
-	}
-)
+// mongoose.connect(
+// 	DB,
+// 	async(err)=>{
+// 			if(err) throw err;
+// 			console.log("DB Connection successful")
+// 	}
+// )
+
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true
+}).then(con => console.log('DB CONNECTION SUCCESSFUL'));
 
 
 
